6192774b95b95018e44d430210463663047dece1
ahmaddraz <aabudraz3@smail.ucas.edu.ps git config --global user.name>
2026-03-28 13:42:29 +0300

    Final exam completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\BookController; /* <-- ضفنا السطر ده عشان نتعرف على الكتب */
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
